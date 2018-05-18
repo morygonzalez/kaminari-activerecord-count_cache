053c2029b565930d8e95d0ce04ea4e5b631f56fb
Hitoshi Nakashima <hitoshi.nakashima@gmail.com>
2018-05-19 07:17:29 +0900

    Initial commit

---
+++ lib/kaminari/activerecord/count_cache.rb
@@ -0,0 +1,9 @@
+require "kaminari/activerecord/count_cache/version"
+
+module Kaminari
+  module Activerecord
+    module CountCache
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/kaminari/activerecord/count_cache/version.rb
@@ -0,0 +1,7 @@
+module Kaminari
+  module Activerecord
+    module CountCache
+      VERSION = "0.1.0"
+    end
+  end
+end
